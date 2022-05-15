237faa166924301e1df64d64ed69d6c104048dae
Shahir Islam <lifeofdekisugi@gmail.com>
2022-05-16 02:48:11 +0600

    Dashboard Top Item

--- app/src/main/java/com/example/blooddonation/MainFragments/FragmentDashboard.java
+++ app/src/main/java/com/example/blooddonation/MainFragments/FragmentDashboard.java
@@ -1,5 +1,7 @@
 package com.example.blooddonation.MainFragments;
 
+import static com.android.volley.VolleyLog.TAG;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,16 +10,27 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.blooddonation.HomeElementContainerActivity;
 import com.example.blooddonation.MainFragments.home.FragmentHome;
 import com.example.blooddonation.MainFragments.prevRequests.fragRequests;
 import com.example.blooddonation.R;
 import com.example.blooddonation.databinding.FragmentDashboardBinding;
 import com.example.blooddonation.databinding.HomeActivityBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class FragmentDashboard extends Fragment {
@@ -43,6 +56,8 @@ public class FragmentDashboard extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
+
+        getDashboardTopItem();
 
         binding.viewBloodRequests.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,5 +88,34 @@ public class FragmentDashboard extends Fragment {
         });
 
         return binding.getRoot();
+    }
+
+    private void getDashboardTopItem() {
+        RequestQueue queue = Volley.newRequestQueue(mContext);
+        String url = "https://blood.dreamitdevlopment.com/public/api/dashboard-counter";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, "onResponse: @@@@@@@@@@@@@             Full Response : " + response);
+
+                try {
+
+                    binding.tvTotalUserOnApp.setText("মোট ইউজারঃ " +  response.getString("totalUser"));
+                    binding.tvTotalBloodDonateOnApp.setText("মোট রক্ত দানঃ " + response.getString("totalDonation"));
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(jsonObjectRequest);
     }
 }
