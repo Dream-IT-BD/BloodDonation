81de068dbbac693d3c4c135f8b0be140a196fb98
Shahir Islam <lifeofdekisugi@gmail.com>
2023-01-02 02:03:09 +0600

    filter-on-blood donation (Without API)

--- app/src/main/java/com/example/blooddonation/MainFragments/home/FragmentHome.java
+++ app/src/main/java/com/example/blooddonation/MainFragments/home/FragmentHome.java
@@ -1,5 +1,7 @@
 package com.example.blooddonation.MainFragments.home;
 
+import static com.android.volley.VolleyLog.TAG;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -16,7 +18,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.android.volley.Request;
@@ -26,6 +31,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.blooddonation.LoadingDialog;
+import com.example.blooddonation.MainFragments.UserProfile.ActivityEditProfile;
 import com.example.blooddonation.databinding.FragHomeBinding;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
@@ -44,6 +50,8 @@ public class FragmentHome extends Fragment {
     Context mContext;
     String token;
     LoadingDialog loadingDialog;
+    private ArrayList<String> upazilas;
+    String userSelectedUpazila;
 
     FragHomeBinding binding;
     private List<BloodRequestItem> bloodRequestItems;
@@ -67,11 +75,35 @@ public class FragmentHome extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragHomeBinding.inflate(inflater,container,false);
 
-        swipeRefreshLayout = binding.fragHomeXML;
+        swipeRefreshLayout = binding.swipeRefreshLayout;
         loadingDialog = new LoadingDialog(mContext);
+        upazilas = new ArrayList<String>();
+
+        getUpazilaData();
 
 
-        binding.fragHomeXML.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+        binding.upazilaSelectorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                userSelectedUpazila = upazilas.get(position);
+                Log.d(TAG, "onItemSelected: @@@@@@@@@@@              User Selected : " + userSelectedUpazila);
+
+                loadingDialog.show();
+                fetchBloodRequestData();
+                bloodRequestItems.clear();
+                binding.tvLoadingDataFor.setText("Loading blood requests for : " + userSelectedUpazila);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d(TAG, "onNothingSelected: @@@@@@@@@@@               Nothing Selected !");
+            }
+        });
+
+
+        binding.swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
 
@@ -146,5 +178,51 @@ public class FragmentHome extends Fragment {
         }) ;
         queue.add(stringRequest);
     }
+
+    private void getUpazilaData(){
+
+        upazilas.clear();
+        //spinnerUpazila.setText(null);
+
+        RequestQueue queue = Volley.newRequestQueue(mContext);
+        String url = "https://blood.dreamitdevlopment.com/public/api/upazila?district_id=বগুড়া";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray jsonArray = jsonObject.getJSONArray("upazila");
+
+                            Log.d(TAG, "onResponse: @@@@@@@" + jsonArray);
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject upazila = jsonArray.getJSONObject(i);
+
+                                upazilas.add(upazila.getString("bn_name"));
+                            }
+
+                            /*for address select*/
+                            ArrayAdapter<String> upazilaAdapter = new ArrayAdapter<>(mContext, android.R.layout.simple_spinner_dropdown_item, upazilas);
+                            //spinnerUpazila.setAdapter(upazilaAdapter);
+                            binding.upazilaSelectorSpinner.setAdapter(upazilaAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse:    " + error);
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+
 
 }
